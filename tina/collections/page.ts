c2b285e15f379cfe414425c17ca7b685f598137a
aido141176 <aiden@amcd.com.au>
2026-04-03 10:29:09 +0700

    test

--- tina/collections/page.ts
+++ tina/collections/page.ts
@@ -21,6 +21,35 @@ export const PageCollection: Collection = {
       type: "rich-text",
       isBody: true,
       required: true
-    }
+    },
+        {
+      type: "object",
+      list: true,
+      name: "blocks",
+      label: "Sections",
+      templates: [
+        {
+          name: "cta",
+          label: "CTA Section",
+          fields: [
+            {
+              type: "string",
+              name: "heading",
+              label: "Heading",
+            },
+            {
+              type: "string",
+              name: "subheading",
+              label: "Subheading",
+            },
+            {
+              type: "string",
+              name: "buttonText",
+              label: "Button Text",
+            },
+          ],
+        },
+      ],
+    },
   ]
 }
