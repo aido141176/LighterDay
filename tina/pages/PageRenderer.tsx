8e0aec9927c0fd09f72d14f713eca094ac097822
aido141176 <aiden@amcd.com.au>
2026-04-04 16:14:32 +0700

    Change HomePage to Page renderer

---
+++ tina/pages/PageRenderer.tsx
@@ -0,0 +1,27 @@
+import { useTina } from "tinacms/dist/react";
+import type { PageQuery, PageQueryVariables } from "../__generated__/types";
+import Page from "./Page";
+
+type Props = {
+  variables: PageQueryVariables;
+  data: PageQuery;
+  query: string;
+};
+
+const PageRenderer = (props: Props) => {
+  const { data } = useTina({
+    query: props.query,
+    variables: props.variables,
+    data: props.data,
+  });
+
+  const page = data?.page;
+
+  if (!page) {
+    return <main>No page data found.</main>;
+  }
+
+  return <Page page={page} />;
+};
+
+export default PageRenderer;
